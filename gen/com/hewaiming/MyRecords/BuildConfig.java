974ea1db1b9ea15c83cc535b3612a9eda1e869a4
Administrator <Administrator@lord>
2017-08-16 14:28:37 +0800

    2017-8-16 统一用android-support-library

---
+++ gen/com/hewaiming/MyRecords/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.hewaiming.MyRecords;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- src/com/hewaiming/MyRecords/charts/BarChartActivity.java
+++ src/com/hewaiming/MyRecords/charts/BarChartActivity.java
@@ -1,6 +1,6 @@
 package com.hewaiming.MyRecords.charts;
 
-import android.support.v7.app.ActionBarActivity;
+
 
 import java.util.ArrayList;
 import java.util.List;
